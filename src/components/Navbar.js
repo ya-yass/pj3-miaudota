f46b4c14ddcef1629d0dd2c70de2317b014558d2
Fernanda Sayuri <fernandasayuri.160@gmail.com>
2023-04-06 17:39:12 -0300

    Navbar vazia

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import './Navbar.css'
+import Box from '@mui/material/Box'
+
+const Navbar = () => {
+    return (
+        <Box component='navbar'>
+            <h3>Logo</h3>
+        </Box>
+    )
+}
+
+export default Navbar
