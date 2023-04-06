494f056ec5ff18644e6c5bc1a35250cafbe931e4
Fernanda Sayuri <fernandasayuri.160@gmail.com>
2023-04-06 17:38:13 -0300

    Rotas do Site

---
+++ src/Router.js
@@ -0,0 +1,16 @@
+import {Route, Routes} from 'react-router-dom'
+import Home from './pages/Home'
+import PerfilUsuario from './pages/PerfilUsuario'
+import Error404 from './pages/Error404'
+
+const Router = () => {
+    return( 
+        <Routes>
+            <Route path="/" element={<Home />} />
+            <Route path="/user" element={<PerfilUsuario />} />
+            <Route path="*" element={<Error404 />} />
+        </Routes>
+    )
+}
+
+export default Router
