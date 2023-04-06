42254bbf1bc3f94517dc4d32d9e4767ffaa5a3bf
Fernanda Sayuri <fernandasayuri.160@gmail.com>
2023-04-06 17:39:35 -0300

    Home

---
+++ src/pages/Home.jsx
@@ -0,0 +1,20 @@
+import Box from '@mui/material/Box'
+import Navbar from '../components/Navbar'
+import Content from '../components/Content'
+import Footer from '../components/Footer'
+
+const Home = () => {
+  return (
+    <>
+      <Navbar />
+      <Box sx={{
+        display: 'flex'
+      }}>
+        <Content />
+      </Box>
+      <Footer />
+    </>
+  )
+}
+
+export default Home
