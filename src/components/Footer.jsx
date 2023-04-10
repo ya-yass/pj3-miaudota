cc1ef6f82f211c9039db96a2ecbbb4b9aee4f5a5
Yasmim Ramos <yasmimrlopes2018@gmail.com>
2023-04-10 15:20:00 -0300

    criação do footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,9 +1,26 @@
 import Box from '@mui/material/Box'
+import './Footer.css'
+import logo from './assets/img/logo-imp.avif'
 
 const Footer = () => {
   return (
-    <Box>
-      <h2>Footer</h2>
+    <Box className="footer">
+      <div className="infoFooter">
+        <img src={logo} className='logo' alt='logo do MiauDota' />
+        <div className="endereco">
+          <span>Av. Bahia, 1739 - Indaiá, Caraguatatuba - SP, 11665-071</span>
+          <span>miaudota@gmail.com.br</span>
+          <span>(00) 0 0000-0000 | (00) 0 0000-0000</span>
+        </div>
+
+        <div className="integrantes">
+          <span>Anita Cristina, email@domain.com</span>
+          <span>Fernanda Sayuri Kano, email@domain.com</span>
+          <span>Maria Eduarda Fabris, email@domain.com</span>
+          <span>Yasmim Ramos Lopes, email@domain.com</span>
+        </div>
+      </div>
+      <span className='copyright'>Copyright © 2023</span>
     </Box>
   )
 }
