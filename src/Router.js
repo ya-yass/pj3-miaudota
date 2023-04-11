94aa2408dff43f3d41c46ec0b443b9fdf4c5c05a
Fernanda Sayuri <fernandasayuri.160@gmail.com>
2023-04-11 16:28:42 -0300

    add page adoção no router

--- src/Router.js
+++ src/Router.js
@@ -2,6 +2,7 @@ import { Route, Routes } from 'react-router-dom'
 import Home from './pages/Home'
 import PerfilUsuario from './pages/PerfilUsuario'
 import Error404 from './pages/Error404'
+import Adocao from './pages/Adocao'
 
 const Router = () => {
     return (
@@ -9,6 +10,7 @@ const Router = () => {
             <Route path="/" element={<Home />} />
             <Route path="/user" element={<PerfilUsuario />} />
             <Route path="*" element={<Error404 />} />
+            <Route path="/adocao" element={<Adocao />} />
         </Routes>
     )
 }
