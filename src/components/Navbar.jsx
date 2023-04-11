529d3d1a6a5904c71dc66f02287ec34a138eebae
Fernanda Sayuri <fernandasayuri.160@gmail.com>
2023-04-11 16:29:35 -0300

    navbar criação dos botoes e user

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,12 +1,34 @@
 import './Navbar.css'
 import Box from '@mui/material/Box'
+import logo from './assets/img/miaudota-logo.png'
+import {Link} from 'react-router-dom'
+import { FaUserAlt as User } from 'react-icons/fa'
+import Home from '../pages/Home'
+
+//TODO perguntar para o prof sobre a rota da logo retornar para Home
 
 const Navbar = () => {
+
     return (
         <Box component='navbar'>
-            <h3>Logo</h3>
+            
+            <a href={Home}><img src={logo} className='logo' alt='logo do MiauDota'/></a>
+            <div>
+                <Link to='/adocao'>
+                    <button type="button" className="btn btn-info">Quero adotar</button>
+                </Link>
+            </div>
+
+            <button type="button" class="btn btn-secundary">
+                Achei um pet
+            </button>
+
+            <div>
+                <User/>
+            </div>
         </Box>
     )
+    
 }
 
 export default Navbar
