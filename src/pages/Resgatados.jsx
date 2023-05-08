427ae08a386a7968d283c091462ac9cb6e47bc0d
Fernanda Sayuri <fernandasayuri.160@gmail.com>
2023-05-08 16:33:58 -0300

    modelo da página de resgatados

---
+++ src/pages/Resgatados.jsx
@@ -0,0 +1,17 @@
+import Box from "@mui/material/Box"
+import Navbar from "../components/Navbar"
+import Footer from "../components/Footer"
+
+const Resgatados = () => {
+  return (
+    <>
+      <Box>
+        < Navbar />
+        <h2>Funcionou Resgatados</h2>
+        <Footer></Footer>
+      </Box>
+    </>
+  )
+}
+
+export default Resgatados
