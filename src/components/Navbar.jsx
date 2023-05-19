bf477120662036989a536b7e20b502eebdfdacc7
Yasmim Ramos <yasmimrlopes2018@gmail.com>
2023-05-19 14:40:42 -0300

    ajuste dos botões da navbar+avatar

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -20,66 +20,65 @@ const settings = ['Entre', 'Cadastre-se'];
 
 
 const Navbar = () => {
-	//abrir e fechar o modal
-	const [open, setOpen] = React.useState(false);
-	const handleOpen = () => setOpen(true);
-	const handleClose = () => setOpen(false);
+  //abrir e fechar o modal
+  const [open, setOpen] = React.useState(false);
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
 
-	const [anchorElUser, setAnchorElUser] = React.useState(null);
+  const [anchorElUser, setAnchorElUser] = React.useState(null);
 
-	const handleOpenUserMenu = (event) => {
-		setAnchorElUser(event.currentTarget);
-	};
+  const handleOpenUserMenu = (event) => {
+    setAnchorElUser(event.currentTarget);
+  };
 
-	const handleCloseUserMenu = () => {
-		setAnchorElUser(null);
-	};
+  const handleCloseUserMenu = () => {
+    setAnchorElUser(null);
+  };
 
 
-	return (
-		<Box component='nav'>
+  return (
+    <Box component='nav'>
 
-			<div className="sec1">
-				<Link to="/">
-					<img src={logo} id='logo1' alt='logo do MiauDota' />
-				</Link>
+      <div className="sec1">
+        <Link to="/">
+          <img src={logo} id='logo1' alt='logo do MiauDota' />
+        </Link>
 
-				<div className="links">
-					<Link to="/" className='pgInicial link' >Página inicial</Link>
-					{/* <a href='*' className='encontrePet'>Encontre seu pet</a> */}
-					<Link to="/resgatados" className='resgatados link'>Resgatados</Link>
-					<Link to="/adocao" className='adocao link'>Adoção</Link>
-					<Link to="/cadastrar-pet" className='cadastroPet link'>Cadastrar um pet</Link>
-				</div>
-			</div>
+        <div className="links">
+          <Link to="/" className='pgInicial link' >Página inicial</Link>
+          {/* <a href='*' className='encontrePet'>Encontre seu pet</a> */}
+          <Link to="/resgatados" className='resgatados link'>Resgatados</Link>
+          <Link to="/adocao" className='adocao link'>Adoção</Link>
+          <Link to="/cadastrar-pet" className='cadastroPet link'>Cadastrar um pet</Link>
+        </div>
+      </div>
 
-			<div id="botoes">
-				<button style={styles.Button} >Login</button>
-					{/* <button className='botao' id='cadastrarPet'>Cadastrar um pet</button> */}
-				<button style={styles.Button} onClick={handleOpen} >Cadastrar-se</button>
-					{/* <button className='botao' id='adotar'>Quero adotar</button> */}
-			
-				{/* Modal Cadastrar-se */}
-			</div>
+      <div id="botoes">
+        <button style={styles.Button} >Login</button>
+        {/* <button className='botao' id='cadastrarPet'>Cadastrar um pet</button> */}
+        <button style={styles.Button} onClick={handleOpen} >Cadastrar-se</button>
+        {/* <button className='botao' id='adotar'>Quero adotar</button> */}
 
-				<Modal
-        open={open}
-        onClose={handleClose}
-        aria-labelledby="modal-modal-title"
-        aria-describedby="modal-modal-description">
+        {/* Modal Cadastrar-se */}
 
-        <Box sx={styles.Modal}>
-          <Cancel className='botaoCancelModal' onClick={handleClose} />
-          <Typography id="modal-modal-title" component="div">
-            <img src={logo} className='logoModal' alt='Logo do Modal'></img>
-            <div>
-              <h3>CADASTRE-SE</h3>
-              <p>Já tem uma conta? Faça seu login</p>
-            </div>
-          </Typography>
+        <Modal
+          open={open}
+          onClose={handleClose}
+          aria-labelledby="modal-modal-title"
+          aria-describedby="modal-modal-description">
 
-          
-					<div className='divTituloCampo tituloCampoNome'>
+          <Box sx={styles.Modal}>
+            <Cancel className='botaoCancelModal' onClick={handleClose} />
+            <Typography id="modal-modal-title" component="div">
+              <img src={logo} className='logoModal' alt='Logo do Modal'></img>
+              <div>
+                <h3>CADASTRE-SE</h3>
+                <p>Já tem uma conta? Faça seu login</p>
+              </div>
+            </Typography>
+
+
+            <div className='divTituloCampo tituloCampoNome'>
               <label className='titulocampoNome'>Nome</label>
               <p>*</p>
             </div>
@@ -93,105 +92,110 @@ const Navbar = () => {
             // onChange={handleFileChange}
             />
 
-          <Typography id="modal-modal-description" component="div"
-            sx={{
-              mt: 2,
-              '& fieldset': {
-                display: 'none',
-              },
-              '& legend': {
-                display: 'none',
-              }
-            }}>
+            <Typography id="modal-modal-description" component="div"
+              sx={{
+                mt: 2,
+                '& fieldset': {
+                  display: 'none',
+                },
+                '& legend': {
+                  display: 'none',
+                }
+              }}>
 
-            <div className='divTituloCampo tituloCampoNome'>
-              <label className='titulocampoNome'>Usuário</label>
-              <p>*</p>
-            </div>
-            <TextField
-              sx={{ borderRadius: '20px', height: '40px', width: '200px', marginBottom: '15px' }}
-              placeholder='Digite seu usuário'
-              type="text"
-              className='input'
-              InputLabelProps={{ shrink: true }}
+              <div className='divTituloCampo tituloCampoNome'>
+                <label className='titulocampoNome'>Usuário</label>
+                <p>*</p>
+              </div>
+              <TextField
+                sx={{ borderRadius: '20px', height: '40px', width: '200px', marginBottom: '15px' }}
+                placeholder='Digite seu usuário'
+                type="text"
+                className='input'
+                InputLabelProps={{ shrink: true }}
 
-            // onChange={handleFileChange}
-            />
+              // onChange={handleFileChange}
+              />
 
-            <div className='divTituloCampo'>
-              <label className='titulocampo'>E-mail</label>
-              <p>*</p>
-            </div>
-            <TextField
-              sx={{ marginTop: '15px' }}
-              placeholder='Digite um e-mail válido'
-              type="text"
-              className='input'
+              <div className='divTituloCampo'>
+                <label className='titulocampo'>E-mail</label>
+                <p>*</p>
+              </div>
+              <TextField
+                sx={{ marginTop: '15px' }}
+                placeholder='Digite um e-mail válido'
+                type="text"
+                className='input'
 
-              InputLabelProps={{ shrink: true }}
+                InputLabelProps={{ shrink: true }}
 
-            // onChange={handleFileChange}
-            />
-            <div className='divTituloCampo'>
-              <label className='titulocampo'>Senha</label>
-              <p>*</p>
-            </div>
-            <TextField
-              sx={{ marginTop: '-10px' }}
-              placeholder='Crie uma senha'
-              type="text"
-              className='input'
+              // onChange={handleFileChange}
+              />
+              <div className='divTituloCampo'>
+                <label className='titulocampo'>Senha</label>
+                <p>*</p>
+              </div>
+              <TextField
+                sx={{ marginTop: '-10px' }}
+                placeholder='Crie uma senha'
+                type="text"
+                className='input'
 
-              InputLabelProps={{ shrink: true }}
+                InputLabelProps={{ shrink: true }}
 
-            // onChange={handleFileChange}
-            />
+              // onChange={handleFileChange}
+              />
 
-            <div className='Botoesmodal'>
-              <ButtonEnviar variant="contained" id='botaoEnviar' >Cadastrar</ButtonEnviar>
-              <p className='botãoCancelarEditar' onClick={handleClose} >Cancelar</p>
-            </div>
+              <div className='Botoesmodal'>
+                <ButtonEnviar variant="contained" id='botaoEnviar' >Cadastrar</ButtonEnviar>
+                <p className='botãoCancelarEditar' onClick={handleClose} >Cancelar</p>
+              </div>
 
-          </Typography>
+            </Typography>
+          </Box>
+
+          {/* Modal Avatar */}
+        </Modal>
+
+        <Box sx={{ flexGrow: 0 }}>
+          <Tooltip title="Open settings">
+            <IconButton onClick={handleOpenUserMenu}
+              className='avatar'
+              sx={{
+                p: 0,
+              }}>
+              <Avatar src="/broken-image.jpg" />
+            </IconButton>
+          </Tooltip>
+          <Menu
+            sx={{ mt: '45px' }}
+            id="menu-appbar"
+            anchorEl={anchorElUser}
+            anchorOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            keepMounted
+            transformOrigin={{
+              vertical: 'top',
+              horizontal: 'right',
+            }}
+            open={Boolean(anchorElUser)}
+            onClose={handleCloseUserMenu}
+          >
+            {settings.map((setting) => (
+              <MenuItem key={setting} onClick={handleCloseUserMenu}>
+                <Typography textAlign="center">{setting}</Typography>
+              </MenuItem>
+            ))}
+          </Menu>
         </Box>
+      </div>
 
-			{/* Modal Avatar */}
-      </Modal>
-				<Box sx={{ flexGrow: 0 }}>
-					<Tooltip title="Open settings">
-						<IconButton onClick={handleOpenUserMenu}
-							className='avatar'
-							sx={{
-								p: 0,
-							}}>
-							<Avatar src="/broken-image.jpg" />
-						</IconButton>
-					</Tooltip>
-					<Menu
-						sx={{ mt: '45px' }}
-						id="menu-appbar"
-						anchorEl={anchorElUser}
-						anchorOrigin={{
-							vertical: 'top',
-							horizontal: 'right',
-						}}
-						keepMounted
-						transformOrigin={{
-							vertical: 'top',
-							horizontal: 'right',
-						}}
-						open={Boolean(anchorElUser)}
-						onClose={handleCloseUserMenu}
-					>
-						{settings.map((setting) => (
-							<MenuItem key={setting} onClick={handleCloseUserMenu}>
-								<Typography textAlign="center">{setting}</Typography>
-							</MenuItem>
-						))}
-					</Menu>
-				</Box>
-		</Box >
-	);
+
+
+    </Box >
+  );
 }
 const styles = {
 
